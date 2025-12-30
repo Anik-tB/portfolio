52621aadfcfee7e7c8b43d3e415ca81c6f0ac8a1
Anik-tB <mdabusayumanik123@gmail.com>
2025-12-30 15:57:38 +0600

    updated

--- script.js
+++ script.js
@@ -156,8 +156,8 @@ const magneticButtons = document.querySelectorAll('.magnetic');
 magneticButtons.forEach(btn => {
     btn.addEventListener('mousemove', (e) => {
         const position = btn.getBoundingClientRect();
-        const x = e.pageX - position.left - position.width / 2;
-        const y = e.pageY - position.top - position.height / 2;
+        const x = e.clientX - position.left - position.width / 2;
+        const y = e.clientY - position.top - position.height / 2;
 
         btn.style.transform = `translate(${x * 0.3}px, ${y * 0.3}px)`;
     });
